3d7794b958100b8b9ed9cc2a6e557dcc2e9dab65
expez <expez@expez.com>
2014-03-10 07:00:15 +0100

    Add lein-localrepo and opencv injection

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[lein-localrepo "0.5.3"]
+                 [org.clojure/clojure "1.5.1"]
                  [opencv/opencv "2.4.8"]
-                 [opencv/opencv-native "2.4.8"]])
+                 [opencv/opencv-native "2.4.8"]]
+  :injections [(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)])
